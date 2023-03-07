767f17e3e82834f4ed6309a583e235ee5b9a2925
Claudio Henrique Soares da Cruz <claudio.dacruz@gmail.com>
2023-03-07 16:14:23 -0500

    Refatoration

--- script.js
+++ script.js
@@ -17,140 +17,81 @@ window.onload = function() {
     listenerInputSoumettre();
 };
 
-let quantiteCours;
-let don;
+let quantiteCours = 0;
+let don = 0;
 
 /*********************************************************/
-/* Validation du nom                                     */
+/* Validation des champs du formulaire                   */
 /*********************************************************/
-function validerNomComplet(regExpNom) {
-    const nom = document.getElementById('nomcomplet');
-    if (nom != null) {    
-        let errorLabel = document.getElementById('validerNom');
-        if(nom.value.match(regExpNom) == null){
+function validerChampFormulaire(regEx, champ,errorLabel) {  
+    if (champ != null) {    
+        if(champ.value.match(regEx) == null){
             errorLabel.style.display="block";
-            nom.focus();
-            nom.value = "";
+            champ.focus();
+            champ.value = "";
         } else {
             errorLabel.style.display="none";
         }
     }  
 }
 
+/*********************************************************/
+/* Validation du champ nom complet                                    */
+/*********************************************************/
 function listenerInputTextNomComplet() {
-    const nom = document.getElementById('nomcomplet');
+    let nom = document.getElementById('nomcomplet');
+    let errorLabel = document.getElementById('validerNom');
     const regExpNom = /(^[A-Z]([a-z]*)[\s]|^[A-Z]([a-z]*)[-][A-Z]([a-z]*)+[\s])+([A-Z]([a-z]*)[\s]|[A-Z]([a-z]*)[-][A-Z]([a-z]*))*/gm;
     nom.addEventListener("focusout", (evt)=> {
-        validerNomComplet(regExpNom);
+        validerChampFormulaire(regExpNom, nom,errorLabel);
     });
 }
 
 /*********************************************************/
 /* Validation du numéro de telephone                     */
 /*********************************************************/
-function validerNumeroTelephone(regExpTel) {
-    const tel = document.getElementById('telephone');
-    
-    if (tel != null) {    
-        let errorLabel = document.getElementById('validerTel');
-        
-        if(tel.value.match(regExpTel) == null){
-            errorLabel.style.display="block";
-            tel.focus();
-            tel.value = "";
-        } else {
-            errorLabel.style.display="none";
-        }
-    }  
-}
-
 function listenerInputNumTelephone() {
-    const tel = document.getElementById('telephone');
+    let tel = document.getElementById('telephone');
+    let errorLabel = document.getElementById('validerTel');
     const regExpTel = /(^\(?\d{3}\)[\s]*\d{3}[-]+\d{4}$|^\d{3}[-]\d{3}[-]+\d{4}$|^\d{3}[.]\d{3}[.]+\d{4}$|\d{10}$)/gm;
     tel.addEventListener("focusout", (evt)=> {
-        validerNumeroTelephone(regExpTel);
+        validerChampFormulaire(regExpTel, tel,errorLabel);
     });
 }
 
 /*********************************************************/
 /* Validation du code postal                             */
 /*********************************************************/
-function validerCodePostal(regExpCodePostal) {
-    const codepostal = document.getElementById('codepostal');
-    console.log(codepostal.value);
-    if (codepostal != null) {    
-        let errorLabel = document.getElementById('validerCodePostal');
-        
-        if(codepostal.value.match(regExpCodePostal) == null){
-            errorLabel.style.display="block";
-            codepostal.focus();
-            codepostal.value = "";
-        } else {
-            errorLabel.style.display="none";
-        }
-    }  
-}
-
 function listenerInputCodePostal() {
-    const codepostal = document.getElementById('codepostal');
-    const regCodePostal = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/gm;
+    let codepostal = document.getElementById('codepostal');
+    let errorLabel = document.getElementById('validerCodePostal');
+    const regExPCodePostal = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/gm;
     codepostal.addEventListener("focusout", (evt)=> {
-        validerCodePostal(regCodePostal);
+        validerChampFormulaire(regExPCodePostal, codepostal,errorLabel);
     });
 }
-
 
 /*********************************************************/
 /* Validation de la date de naissance                    */
 /*********************************************************/
-function  validerDateNaissance(regDate) {
-    const datenaissance = document.getElementById('datenaissance');
-   
-    if (datenaissance != null) {    
-        let errorLabel = document.getElementById('validerDateNaissance');
-        
-        if(datenaissance.value.match(regDate) == null){
-            errorLabel.style.display="block";
-            datenaissance.focus();
-            datenaissance.value = "";
-        } else {
-            errorLabel.style.display="none";
-        }
-    }  
-}
-
 function listenerInputDateNaissance() {
-    const date = document.getElementById('datenaissance');
-    const regDate = /((\d{4})[-.\/](0[1-9]|1[0-2])[-\/]([1-9]|[1-2][0-9]|3[0-1])$)|(^([1-9]|[1-2][0-9]|3[0-1])[ ]([A-Za-z])+[ ](\d{4}))$/gm;
+    let date = document.getElementById('datenaissance');
+    let errorLabel = document.getElementById('validerDateNaissance');
+    const regExDate = /((\d{4})[-.\/](0[1-9]|1[0-2])[-\/]([1-9]|[1-2][0-9]|3[0-1])$)|(^([1-9]|[1-2][0-9]|3[0-1])[ ]([A-Za-z])+[ ](\d{4}))$/gm;
     date.addEventListener("focusout", (evt)=> {
-        validerDateNaissance(regDate);
+        validerChampFormulaire(regExDate,date,errorLabel);
     });
 }
 
 /*********************************************************/
 /* Validation du numero NAS                              */
 /*********************************************************/
-function  validerNAS(regNAS) {
-    const nas = document.getElementById('codenas');
-    
-    if (nas != null) {    
-        let errorLabel = document.getElementById('validerNAS');
-        
-        if(nas.value.match(regNAS) == null){
-            errorLabel.style.display="block";
-            nas.focus();
-            nas.value = "";
-        } else {
-            errorLabel.style.display="none";
-        }
-    }  
-}
-
 function listenerInputNAS() {
-    const nas = document.getElementById('codenas');
-    const regNAS = /^(\d{3})[-](\d{3})[-](\d{3})[-]*|^(\d{3})[ ](\d{3})[ ]*(\d{3})[ ]*|\d{9}$/gm;
+    let nas = document.getElementById('codenas');
+    let errorLabel = document.getElementById('validerNAS')
+    const regExNAS = /^(\d{3})[-](\d{3})[-](\d{3})[-]*|^(\d{3})[ ](\d{3})[ ]*(\d{3})[ ]*|\d{9}$/gm;
     nas.addEventListener("focusout", (evt)=> {
-        validerNAS(regNAS);
+        validerChampFormulaire(regExNAS,nas,errorLabel);
     });
 }
 
@@ -163,7 +104,7 @@ function  validerCourriel(regCourriel) {
     
     if (courriel != null) {    
         let errorLabel = document.getElementById('validerCourriel');
-        
+        console.log("email");
         if(courriel.value.match(regCourriel) == null){
             errorLabel.style.display="block";
             courriel.focus();
@@ -175,7 +116,7 @@ function  validerCourriel(regCourriel) {
 }
 
 function listenerInputCourriel() {
-    const courriel = document.getElementById('courriel');
+    let courriel = document.getElementById('courriel');
     const regCourriel = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gm;
     courriel.addEventListener("focusout", (evt)=> {
         validerCourriel(regCourriel);
@@ -185,39 +126,23 @@ function listenerInputCourriel() {
 /*********************************************************/
 /* Validation de l'adresse internet                      */
 /*********************************************************/
-function  validerAdresseInternet(regAdresseInternet) {
-    const adresseInternet = document.getElementById('adressinternet');
-    
-    if (adresseInternet != null) {    
-        let errorLabel = document.getElementById('validerAdresseInternet');
-        
-        if(adresseInternet.value.match(regAdresseInternet) == null){
-            errorLabel.style.display="block";
-            adresseInternet.focus();
-            adresseInternet.value = "";
-        } else {
-            errorLabel.style.display="none";
-        }
-    }  
-}
-
 function listenerInputCourriel() {
-    const adresseInternet = document.getElementById('adressinternet');
+    let adresseInternet = document.getElementById('adressinternet');
+    let errorLabel = document.getElementById('validerAdresseInternet');
     const regAdresseInternet = /^(ftp)[:][\/][\/](25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$|^(http)[s]*[:]+[\/][\/][www]+[.]([a-z0-9_]+)[.]([a-z]+)/gm;
     adresseInternet.addEventListener("focusout", (evt)=> {
-        validerAdresseInternet(regAdresseInternet);
+        validerChampFormulaire(regAdresseInternet,adresseInternet,errorLabel);
     });
 }
 
 /*********************************************************/
-/* Validation du code de oours                           */
+/* Validation du code de cours                           */
 /*********************************************************/
 function  validerCodeCours(regExCours) {
-    const codecours = document.getElementById('cours');
+    let codecours = document.getElementById('cours');
     if (codecours != null) {
         arrayCours = codecours.value.split(" ");
         quantiteCours = arrayCours.length;
-       
         let coursValide = true;
         for (let index = 0; index < arrayCours.length; index++) {
             let element = arrayCours[index];
@@ -279,89 +204,43 @@ function listenerInputDonnation() {
 }
 
 /*********************************************************/
-/* Validation du type de carte                              */
+/* Validation du type de carte                           */
 /*********************************************************/
-function  validerTypeCarte(regExTypeCarte) {
-    const typecarte = document.getElementById('typecarte');
-    
-    if (typecarte != null) {    
-        let errorLabel = document.getElementById('validerTypeCarte');
-        
-        if(typecarte.value.match(regExTypeCarte) == null){
-            errorLabel.style.display="block";
-            typecarte.focus();
-            typecarte.value = "";
-        } else {
-            errorLabel.style.display="none";
-        }
-    }  
-}
-
 function listenerInputTypeCarte() {
-    const typecarte = document.getElementById('typecarte');
+    let typecarte = document.getElementById('typecarte');
+    let errorLabel = document.getElementById('validerTypeCarte');
     const regExTypeCarte = /((VISA)|(MASTERCARD)|(AMEX))$/gmi;
     typecarte.addEventListener("focusout", (evt)=> {
-        validerTypeCarte(regExTypeCarte);
+        validerChampFormulaire(regExTypeCarte, typecarte,errorLabel);
     });
 }
 
 /*********************************************************/
 /* Validation du numéro de la carte credit               */
 /*********************************************************/
-function  validerNumeroCarte(regExNumeroCarte) {
-    const carte = document.getElementById('numerocarte');
-    
-    if (carte != null) {    
-        let errorLabel = document.getElementById('validerNumeroCarte');
-        
-        if(carte.value.match(regExNumeroCarte) == null){
-            errorLabel.style.display="block";
-            carte.focus();
-            carte.value = "";
-        } else {
-            errorLabel.style.display="none";
-        }
-    }  
-}
-
 function listenerInputNumeroCarte() {
-    const carte = document.getElementById('numerocarte');
+    let carte = document.getElementById('numerocarte');
+    let errorLabel = document.getElementById('validerNumeroCarte');
     const regExNumeroCarte = /^((4540)|(3520)|(5312))([-]+\d{4}[-]+\d{4}[-]+\d{4}|\d{12})$/gm;
     carte.addEventListener("focusout", (evt)=> {
-        validerNumeroCarte(regExNumeroCarte);
+        validerChampFormulaire(regExNumeroCarte, carte,errorLabel);
     });
 }
 
 /*********************************************************/
 /* Validation de la date de expiration de la carte credit*/
 /*********************************************************/
-function  validerDateExpiration(regExDateExpiration) {
-    const dateexpiration = document.getElementById('expirationcarte');
-    
-    if (dateexpiration != null) {    
-        let errorLabel = document.getElementById('validerDateExpiration');
-        
-        if(dateexpiration.value.match(regExDateExpiration) == null){
-            errorLabel.style.display="block";
-            dateexpiration.focus();
-            dateexpiration.value = "";
-        } else {
-            errorLabel.style.display="none";
-        }
-    }  
-}
-
 function listenerInputDateExpiration() {
-    const dateexpiration = document.getElementById('expirationcarte');
+    let dateexpiration = document.getElementById('expirationcarte');
+    let errorLabel = document.getElementById('validerDateExpiration');
     const regExDateExpiration = /^(0[0-9]|1[0-2])[\/]((2[3-9])|([3-9][0-9]))$/gm;
     dateexpiration.addEventListener("focusout", (evt)=> {
-        validerDateExpiration(regExDateExpiration);
+        validerChampFormulaire(regExDateExpiration, dateexpiration, errorLabel);
         calculerAfficherMontant();
     });
 }
 
 function calculerAfficherMontant() {
-    //let donnation = parseInt(document.getElementById('donfondation').value);
     let montantTotal = document.getElementById('montantTotal');
     let montant = quantiteCours * 50 + don;
     montantTotal.innerHTML = montant;
@@ -370,27 +249,12 @@ function calculerAfficherMontant() {
 /*********************************************************/
 /* Validation du mot de passe                            */
 /*********************************************************/
-function   validerMotDePasse(regExMotDePasse) {
-    const mdp = document.getElementById('mpd');
-    
-    if (mdp != null) {    
-        let errorLabel = document.getElementById('validerMotDePasse');
-        
-        if(mdp.value.match(regExMotDePasse) == null){
-            errorLabel.style.display="block";
-            mdp.focus();
-            mdp.value = "";
-        } else {
-            errorLabel.style.display="none";
-        }
-    }  
-}
-
 function listenerInputMotDePasse() {
-    const mdp = document.getElementById('mpd');
+    let mdp = document.getElementById('mpd');
+    let errorLabel = document.getElementById('validerMotDePasse');
     const regExMotDePasse = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[A-Z])(?=.*[_!@#$%&*()\\\/?])[a-zA-Z0-9_!@#$%&*()\\\/?]+$/gm;
     mdp.addEventListener("focusout", (evt)=> {
-        validerMotDePasse(regExMotDePasse);
+        validerChampFormulaire(regExMotDePasse, mdp, errorLabel);
     });
 }
 
@@ -417,4 +281,3 @@ function listenerInputSoumettre() {
         validerformulaireComplet() ;
     });
 }
-
